18f18f22bee35f3ba1655bca6d0199efad501c02
DevAgma <153858125+DevAgma@users.noreply.github.com>
2024-03-07 15:09:21 +0000

    Create fastsetup.user.js

---
+++ fastsetup.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         fastsetup
+// @namespace    fastsetup
+// @version      0.1
+// @description  x
+// @author       x
+// @license      MIT
+// @icon         agma.io/c3.png
+// @match        *://agma.io/*
+// @grant        none
+// @downloadURL  https://github.com/DevAgma/fastsetup/main/fastsetup.user.js
+// @updateURL    https://github.com/DevAgma/fastsetup/main/fastsetup.user.js
+// ==/UserScript==
+source
